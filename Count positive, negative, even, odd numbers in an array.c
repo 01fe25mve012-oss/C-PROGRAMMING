43da1e7e34b8fe8320ef495a9a3aee4d4e1dfe57
01fe25mve012-oss <01fe25mve012@kletech.ac.in>
2025-12-14 21:27:26 +0530

    Add files via upload

---
+++ Count positive, negative, even, odd numbers in an array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int pos = 0, neg = 0, even = 0, odd = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if (arr[i] > 0)
+            pos++;
+        else if (arr[i] < 0)
+            neg++;
+
+        if (arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Positive numbers = %d\n", pos);
+    printf("Negative numbers = %d\n", neg);
+    printf("Even numbers     = %d\n", even);
+    printf("Odd numbers      = %d\n", odd);
+
+    return 0;
+}
