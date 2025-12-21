15d0118f3daa8ab0552572d2b6b5ff23dcc4dfae
01fe25mve012-oss <01fe25mve012@kletech.ac.in>
2025-12-21 13:40:44 +0530

    Add files via upload

---
+++ kth smallest and kth largest.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void sort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[100], n, k;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    if (k > n || k <= 0) {
+        printf("Invalid value of k\n");
+        return 0;
+    }
+
+    sort(arr, n);
+
+    printf("Kth smallest element: %d\n", arr[k - 1]);
+    printf("Kth largest element: %d\n", arr[n - k]);
+
+    return 0;
+}
+
